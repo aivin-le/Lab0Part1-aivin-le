d0f34c4fe00c655e8b529eb6caf0ccff490b203e
aivin-le <at12le@siena.edu>
2026-09-08 16:59:52 -0400

    Added name, hometown, and favorite resturant to printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,15 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Aivin Le
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("My name is Aivin Le.");
+        System.out.println("My hometown is Clifton Park, NY.");
+        System.out.println("My favorite resturant in clifton Park is Pita and Olive.");
     }
 }
