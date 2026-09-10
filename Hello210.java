0554ad60111ef70b97e978476349ba1c794fbf86
aivin-le <at12le@siena.edu>
2026-09-10 08:42:11 -0400

    Added favorite resturaunt.

--- Hello210.java
+++ Hello210.java
@@ -11,6 +11,6 @@ public class Hello210 {
         System.out.println("Hello CSIS 210 World!");
         System.out.println("My name is Aivin Le.");
         System.out.println("My hometown is Clifton Park, NY.");
-        System.out.println("My favorite resturant in clifton Park is Pita and Olive.");
+        System.out.println("My favorite resturant in clifton Park is Pita and Olive. I enjoy eating their falafel.");
     }
 }
